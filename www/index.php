0b3655b46a54d64f324d721d0b451840ba6124fd
clauderech <co.rech@hotmail.com>
2023-06-08 14:28:39 -0300

    atualizacao_08-06-23

--- www/index.php
+++ www/index.php
@@ -16,32 +16,76 @@
     <title>Home</title>
     <link rel="stylesheet" href="css/geral_style.css" />
     <link rel="stylesheet" href="css/style.css" />
-    
+    <link rel="stylesheet" href="css/slide.css" />
 </head>
     <body>
-    <header>
-        <div class="header-title">
-            <!-- <img src="images/logo_snackbar1.png" class="logotipo"> --> 
-            <h1>Snack Bar Lanches</h1>
+        <header>
+            <div class="header-title">
+                <!-- <img src="images/logo_snackbar1.png" class="logotipo"> --> 
+                <h1>Snack Bar Lanches</h1>
+            </div>
+                    
+            <nav>            
+                <div class="menu_mobile" onclick=" menuShow() ">
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+                <ul>
+                    <li><a class="nav-link" href="cardapio.php">Cardápio</a></li>
+                    <li><a class="nav-link" href="cadastro.php">Cadastro</a></li>
+                    <li><a class="nav-link" href="login.php">Entrar</a></li>
+                </ul>
+                <div class="user" style="display: flex;">
+                    <div class="user_name" style="margin-top: 10%; color:aliceblue"><?php echo $nome; ?></div>
+                    <img src="images/user.png" >
+                </div>
+            </nav>
+        </header> 
+        
+        <div class="slider">
+            <div class="slides">
+                <!--Radio Buttons-->
+                <input type="radio" name="radio-btn" id="radio1">
+                <input type="radio" name="radio-btn" id="radio2">
+                <input type="radio" name="radio-btn" id="radio3">
+                <input type="radio" name="radio-btn" id="radio4">
+                <!--Fim Radio Buttons-->
+
+                <!--Slide images-->
+                <div class="slide first">
+                    <img src="images/xisbanner1.jpg" alt="Xis 1">
+                </div>
+                <div class="slide">
+                    <img src="images/xis_banner2.jpg" alt="Xis 2">
+                </div>
+                <div class="slide">
+                    <img src="images/xis_banner3.jpg" alt="Xis 3">
+                </div>
+                <div class="slide">
+                    <img src="images/banner.png" alt="banner 1">
+                </div>
+                <!--Fim Slide images-->
+
+                <!--Navigation auto-->
+                <div class="navigation-auto">
+                    <div class="auto-btn1"></div>
+                    <div class="auto-btn2"></div>
+                    <div class="auto-btn3"></div>
+                    <div class="auto-btn4"></div>
+                </div>
+            </div>
+
+            <div class="manual-navigation">
+                <label for="radio1" class="manual-btn"></label>
+                <label for="radio2" class="manual-btn"></label>
+                <label for="radio3" class="manual-btn"></label>
+                <label for="radio4" class="manual-btn"></label>
+            </div>
+
         </div>
-                
-        <nav>            
-            <div class="menu_mobile" onclick=" menuShow() ">
-                <div></div>
-                <div></div>
-                <div></div>
-            </div>
-            <ul>
-                <li><a class="nav-link" href="cardapio.php">Cardápio</a></li>
-                <li><a class="nav-link" href="cadastro.php">Cadastro</a></li>
-                <li><a class="nav-link" href="login.php">Entrar</a></li>
-            </ul>
-            <div class="user" style="display: flex;">
-                <div class="user_name" style="margin-top: 10%; color:aliceblue"><?php echo $nome; ?></div>
-                <img src="images/user.png" >
-            </div>
-        </nav>
-    </header>            
+
+        <script src="js/slide.js"></script>
         <script src="js/menuRespon.js"></script>
     </body>
 </html>
